984b46675f7ec2edeb325d73115709528fa28574
dlascombe <d.lascombe@neos-interactive.com>
2011-09-29 14:43:31 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+gem 'will_paginate', '3.0.pre2'
+
+group :development do
+  gem 'annotate', '2.4.0'
+  gem 'faker', '0.3.1'
+end
+
+group :test do
+  gem 'webrat', '0.7.1'
+  gem 'spork', '0.9.0.rc8'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brosseau::Application.config.secret_token = '6c7fc3d03aa8f4330f1f195d25d08874d2ec575df492446545cefea1e1888e48036df4418ac0ef22aa23809699f30d2eaccbc0c9c9ebec2658dee2e5cc289996'
